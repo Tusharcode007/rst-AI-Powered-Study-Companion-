404e4eff5ddb8b7c182caa5d513bb149e60d7988
R S Tushar <tusharsmac@Rs-MacBook-Air.local>
2026-03-24 11:04:08 +0530

    STEP 1.2 — WRAP APP

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { StudyProvider } from './context/StudyContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StudyProvider>
+      <App />
+    </StudyProvider>
   </React.StrictMode>,
 )
